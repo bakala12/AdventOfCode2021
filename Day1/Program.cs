5b62d6a0dc781ca22681c55205193f652f59152e
Mateusz Bąkała <maba@netcompany.com>
2021-12-01 20:51:41 +0100

    Initial commit

---
+++ Day1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
+        {
+            Console.WriteLine("part1");
+        }
+
+        private static void Part2()
+        {
+            Console.WriteLine("part2");
+        }
+    }
+}
