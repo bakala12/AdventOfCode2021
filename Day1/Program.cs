03f8864f5ae2b5caa916a74a3635a90d72217821
Mateusz Bąkała <maba@netcompany.com>
2021-12-02 08:25:51 +0100

    Day 1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,21 +1,34 @@
-﻿namespace Day1
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
 {
     public class Program
     {
         public static void Main()
         {
-            Part1();
-            Part2();
+            var input = File.ReadAllLines("input.txt").Select(int.Parse).ToArray();
+            Part1(input);
+            Part2(input);
         }
 
-        private static void Part1()
+        private static void Part1(int[] input)
         {
-            Console.WriteLine("part1");
+            int s = 0;
+            for(int i = 1; i<input.Length; i++)
+                if(input[i] > input[i-1])
+                    s++;
+            Console.WriteLine(s);
         }
 
-        private static void Part2()
+        private static void Part2(int[] input)
         {
-            Console.WriteLine("part2");
+            int s = 0;
+            for(int i = 3; i<input.Length; i++)
+                if(input[i] > input[i-3])
+                    s++;
+            Console.WriteLine(s);
         }
     }
 }
