01f33e601c8d4f4c08588448a2a456c26ed0ba00
Mateusz Bąkała <maba@netcompany.com>
2021-12-07 09:58:56 +0100

    Day 7

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day7
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var nums = File.ReadAllText("input.txt").Split(',').Select(int.Parse).ToArray();
+            Part1(nums);
+            Part2(nums);
+        }
+
+        private static void Part1(int[] numbers)
+        {
+            int min = int.MaxValue;
+            for(int x = numbers.Min(); x <= numbers.Max(); x++)
+            {
+                var y = numbers.Sum(xi => Math.Abs(x - xi));
+                if(y < min)
+                    min = y;
+            }
+            Console.WriteLine(min);
+        }
+
+        private static void Part2(int[] numbers)
+        {
+            int min = int.MaxValue;
+            for(int x = numbers.Min(); x <= numbers.Max(); x++)
+            {
+                var y = numbers.Sum(xi => (Math.Abs(x - xi)+1)*(Math.Abs(x-xi)) / 2);
+                if(y < min)
+                    min = y;
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
